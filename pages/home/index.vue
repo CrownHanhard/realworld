ef62b4cd60c5103254c9e8a5c7ba8b7dd2221997
技术宅王大傻 <15592191450@163.com>
2021-12-18 22:31:33 +0800

    ad

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">自动化部署v1.0</h1>
+        <h1 class="logo-font">自动化部署v1.0.3</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
