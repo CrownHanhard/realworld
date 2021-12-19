d0719d0105f4416c101b48b92ea99655c278b4f0
技术宅王大傻 <15592191450@163.com>
2021-12-19 15:09:03 +0800

    ad

---
+++ .nuxt/dist/server/2.js
@@ -0,0 +1,205 @@
+exports.ids = [2];
+exports.modules = {
+
+/***/ 27:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "f", function() { return getArticles; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "h", function() { return getYourFeedArticles; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return createArticles; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return addFavorite; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "d", function() { return deleteFavorite; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "e", function() { return getArticle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "g", function() { return getComments; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return addComments; });
+/* harmony import */ var _plugins_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5);
+ // 获取公共文章列表
+
+const getArticles = params => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: '/api/articles',
+    params
+  });
+}; // 获取公共文章列表
+
+const getYourFeedArticles = params => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: '/api/articles/feed',
+    params
+  });
+}; // 创建文章
+
+const createArticles = data => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: '/api/articles',
+    data
+  });
+}; // 添加点赞
+
+const addFavorite = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'POST',
+    url: `/api/articles/${slug}/favorite`
+  });
+}; // 取消点赞
+
+const deleteFavorite = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'DELETE',
+    url: `/api/articles/${slug}/favorite`
+  });
+}; // 获取文章详情
+
+const getArticle = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: `/api/articles/${slug}`
+  });
+}; // 获取文章评论
+
+const getComments = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: `/api/articles/${slug}/comments`
+  });
+}; // 发表文章评论
+
+const addComments = (slug, data) => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'POST',
+    url: `/api/articles/${slug}/comments`,
+    data
+  });
+};
+
+/***/ }),
+
+/***/ 35:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/_vue-loader@15.9.8@vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/_vue-loader@15.9.8@vue-loader/lib??vue-loader-options!./pages/editor/index.vue?vue&type=template&id=76631dea&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"editor-page"},[_vm._ssrNode("<div class=\"container page\" data-v-76631dea><div class=\"row\" data-v-76631dea><div class=\"col-md-10 offset-md-1 col-xs-12\" data-v-76631dea><form data-v-76631dea><fieldset data-v-76631dea><fieldset class=\"form-group\" data-v-76631dea><input placeholder=\"Article Title\" type=\"text\""+(_vm._ssrAttr("value",(_vm.article.title)))+" class=\"form-control form-control-lg\" data-v-76631dea></fieldset> <fieldset class=\"form-group\" data-v-76631dea><input placeholder=\"What's this article about?\" type=\"text\""+(_vm._ssrAttr("value",(_vm.article.description)))+" class=\"form-control\" data-v-76631dea></fieldset> <fieldset class=\"form-group\" data-v-76631dea><textarea placeholder=\"Write your article (in markdown)\" rows=\"8\" class=\"form-control\" data-v-76631dea>"+_vm._ssrEscape(_vm._s(_vm.article.body))+"</textarea></fieldset> <fieldset class=\"form-group\" data-v-76631dea><input placeholder=\"Enter tags separated by ,\" type=\"text\""+(_vm._ssrAttr("value",(_vm.article.tagList)))+" class=\"form-control\" data-v-76631dea> <div class=\"tag-list\" data-v-76631dea></div></fieldset> <button type=\"button\" class=\"btn btn-lg pull-xs-right btn-primary\" data-v-76631dea>\n              Publish Article\n            </button></fieldset></form></div></div></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/editor/index.vue?vue&type=template&id=76631dea&scoped=true&
+
+// EXTERNAL MODULE: ./api/article.js
+var article = __webpack_require__(27);
+
+// CONCATENATED MODULE: ./node_modules/_babel-loader@8.2.3@babel-loader/lib??ref--2-0!./node_modules/_vue-loader@15.9.8@vue-loader/lib??vue-loader-options!./pages/editor/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ var editorvue_type_script_lang_js_ = ({
+  middleware: 'authenticated',
+  name: "editorIndex",
+
+  data() {
+    return {
+      article: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: ''
+      }
+    };
+  },
+
+  methods: {
+    async onSubmit() {
+      const status = this.isEmpty(this.article);
+
+      if (!status) {
+        alert("请核实内容后在提交");
+        return false;
+      }
+
+      const _data = { ...this.article,
+        tagList: this.article.tagList.split(",")
+      };
+      await Object(article["c" /* createArticles */])(_data).then(() => {
+        this.$router.push('/');
+      });
+    },
+
+    isEmpty(obj) {
+      let res = 1;
+      Array.from(Object.entries(obj)).forEach(val => {
+        res &= !!val[1];
+      });
+      return !!res;
+    }
+
+  }
+});
+// CONCATENATED MODULE: ./pages/editor/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_editorvue_type_script_lang_js_ = (editorvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/_vue-loader@15.9.8@vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/editor/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_editorvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "76631dea",
+  "dd07b12a"
+  
+)
+
+/* harmony default export */ var editor = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=2.js.map
